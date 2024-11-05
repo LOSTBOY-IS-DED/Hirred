11b38be35cdbc7cc19abcb4eda8dbad0aaeab797
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-05 12:55:42 +0530

    Layout Made Successfully

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -5,10 +5,11 @@ const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <main className=" min-h-screen container border border-red-300">
+      <main className=" min-h-screen container">
         <Header />
         <Outlet />
       </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">Made By Subh 💀</div>
     </div>
   );
 };
