dd33995900ee084442b0851dd63b1bca2d0281e4
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-11 21:47:00 +0530

    created a custom hook useFetch for getting jobs

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,22 +1,18 @@
 // Page for our job listings
 
 import { getJobs } from "@/api/apiJobs";
-import { useSession } from "@clerk/clerk-react";
+import useFetch from "@/hooks/useFetch";
 import { useEffect } from "react";
 
 const JobListing = () => {
-  const { session } = useSession();
-
-  const fetchJobs = async () => {
-    const supabaseAccessToken = await session.getToken({
-      template: "supabase",
-    });
-    const data = await getJobs(supabaseAccessToken);
-    console.log(data);
-  };
+  const {
+    fn: fnJobs,
+    data: dataJobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, {});
 
   useEffect(() => {
-    fetchJobs();
+    fnJobs();
   }, []);
 
   return <div>Job listing </div>;
