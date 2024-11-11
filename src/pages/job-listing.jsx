a83971ac6b6510ce3863d3a5493be2bebdfb60fa
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-11 21:21:30 +0530

    made a function to fetch jobs from the supabase

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,6 +1,24 @@
 // Page for our job listings
 
+import { getJobs } from "@/api/apiJobs";
+import { useSession } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
 const JobListing = () => {
+  const { session } = useSession();
+
+  const fetchJobs = async () => {
+    const supabaseAccessToken = await session.getToken({
+      template: "supabase",
+    });
+    const data = await getJobs(supabaseAccessToken);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return <div>Job listing </div>;
 };
 
